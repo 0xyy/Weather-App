1fcc0641e00a0e09d5651a0a61835280aed4c3d6
Tommy Phan <70032717+0xyy@users.noreply.github.com>
2021-08-05 23:15:26 +0200

    add title animation

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const convertToCelsius = temp => Math.floor(temp - 273.15);
 const convertToKilPerHour = speed => Math.ceil(speed * 18 / 5);
 
 const showWeather = data => {
-    const { main, description, icon } = data.weather[0];
+    const { main, description } = data.weather[0];
     const { name, timezone } = data;
     const { temp, humidity } = data.main;
     const { country } = data.sys;
@@ -63,4 +63,56 @@ const getCity = e => {
 };
 
 submitForm.addEventListener('submit', getCity);
+
+let ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime.timeline({loop: true})
+    .add({
+        targets: '.ml4 .letters-1',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+}).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+}).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+}).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+}).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+}).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+});
 
